c092ebd15d3279f9de098bce2421a178ecac6f24
bolognini <mastheusbolognini79@gmail.com>
2019-09-01 10:54:29 -0300

    Card testing

--- src/card/card.js
+++ src/card/card.js
@@ -3,27 +3,54 @@ import css from './style.css'
 const template = document.createElement('template')
 template.innerHTML = `
   <style>${css}</style>
-  <div>
+  <div class='holder'>
     <slot name='name'></slot>
-    <slot name='id'></slot>
-    <slot name='type0'></slot>
-    <slot name='type1'></slot>
+    <div class='id'></div>
+    <div class='types'>
+      <slot name='type0'></slot>
+      <slot name='type1'></slot>
+    </div>
+    <img />
   </div>
 `
 
+{/* <div>
+  <slot name='name'></slot>
+  <slot name='id'></slot>
+  <slot name='type0'></slot>
+  <slot name='type1'></slot>
+  <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png' />
+</div> */}
+
+{/* <pokedex-card>
+  <span slot='name'>{pokemon.name}</span>
+  <span slot='id'>#{pokemon.id}</span>
+  {pokemon.types && pokemon.types.map((item, index) => <span slot={`type${index}`}>{item.type.name}</span>)}
+</pokedex-card> */}
+
 class Card extends HTMLElement {
   static get observedAttributes() {
-    return ['disabled']
+    return ['name', 'id', 'source']
   }
 
   constructor() {
     super()
     this._shadowRoot = this.attachShadow({ mode: 'open' })
     this._shadowRoot.appendChild(template.content.cloneNode(true))
+    this._id = this._shadowRoot.querySelector('.id')
+    this._source = this._shadowRoot.querySelector('img')
   }
 
   attributeChangedCallback(name, _, newValue) {
     this[name] = newValue
+  }
+
+  set id(value) {
+    this._id.innerHTML = `#${value}`
+  }
+
+  set source(value) {
+    this._source.setAttribute('src', value)
   }
 }
 
--- src/card/card.stories.js
+++ src/card/card.stories.js
@@ -10,6 +10,20 @@ storiesOf('Pokedex Card', module)
   .addDecorator(withNotes)
   .add('Default', () => {
     const el = document.createElement('pokedex-card')
+    
+    const name = text('Name', 'Gengar')
+    const type0 = text('Type 0', 'Poison')
+    const type1 = text('Type 1', 'Ghost')
+    const source = text('Image', 'https://bit.ly/2LdntMS')
+
+    el.id = text('Id', '094')
+    el.source = source
+
+    el.innerHTML = `
+      <span slot='name'>${name}</span>
+      <span slot='type0'>${type0}</span>
+      <span slot='type1'>${type1}</span>
+    `
 
     return el
   }, {
